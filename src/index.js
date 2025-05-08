5cd1c29a4dcf9b4ace2d58cb69b80d2c145c7a68
Ludovico Cammarata <cammarataludvico9@gmail.com>
2025-05-08 10:24:35 +0200

    Base nodemailer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ const express = require("express");
 const server = express();
 const port = 3000;
 
+const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const transporter = nodemailer.createTransport({
+    service: process.env.SMTP_SERVICE,
+    auth: {
+        user: process.env.SMTP_MAIL,
+        pass: process.env.SMTP_PASS
+    }
+}); 
+
+const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: '',
+    subject: 'Test Email',
+    text: 'Hello, this is a test email sent using Nodemailer and Gmail.'
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log('Error sending email: ', error);
+    }
+    console.log('Email sent: ', info.response);
+});
+
 server.listen(port, () => {
     console.log("Server in Ascolto!")
 })
